7b7c4fa1164be2329d7d89ada223b23f2f5d9051
rekinyfilmowe <system@rekinyfilmowe.pl>
2025-11-27 09:54:00 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -198,7 +198,7 @@ document.getElementById("zgodaMarketingowa").textContent =
 function autoPDF() {
   html2pdf()
   .set({
-      margin: [10, 10, 10, 10],   // góra, prawo, dół, lewo w mm
+      margin: 0,
       filename: "oferta.pdf",
       html2canvas: {
           scale: 3,
